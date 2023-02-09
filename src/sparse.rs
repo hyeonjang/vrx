67522507a1b8e0771ef3ab352853f3ac540a31df
hyeonjang <hyeonjang_an@naver.com>
2023-02-09 16:55:28 +0900

    add sparse struct

--- src/sparse.rs
+++ src/sparse.rs
@@ -1,11 +1,37 @@
+use std::marker::PhantomData;
+use std::ops::Index;
+
 pub enum SparesMatrixType {
-    COO, 
-    CSR,
-    CRS,
+    CSC = 0,
 }
 
-pub struct SpareMatrix<T> {
+pub trait SparesMatrixTypeOp {}
+
+pub struct Storage {}
+
+pub struct SparseMatrix<T, U: SparesMatrixTypeOp> {
     val: Vec<T>,
     col_index: Vec<usize>,
     row_index: Vec<usize>,
+    _phantom: PhantomData<U>,
+}
+
+impl<T, U> Index<(usize, usize)> for SparseMatrix<T, U> where U: SparesMatrixTypeOp {
+    type Output = T;
+
+    fn index(&self, row_col:(usize, usize)) -> &Self::Output {
+        &self.val[self.col_index[row_col.0]]
+    }
+}
+
+pub trait SparseSolver {
+    fn factorize();
+}
+
+#[cfg(test)] 
+mod tests {
+    #[test]
+    fn works() {
+        println!("some");
+    }
 }
